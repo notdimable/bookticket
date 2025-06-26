4caa7d6676fae23d9bcdfad1e3fe1c9cd041fe53
notdimable <166415819+Dimassis@users.noreply.github.com>
2025-06-26 14:02:08 +0500

    add "admin-service"

--- src/main/java/com/bookticket/app/api/users/controller/UserController.java
+++ src/main/java/com/bookticket/app/api/users/controller/UserController.java
@@ -1,13 +1,11 @@
 package com.bookticket.app.api.users.controller;
 
 import com.bookticket.app.api.users.model.dto.UserDto;
-import com.bookticket.app.api.users.model.entity.User;
 import com.bookticket.app.api.users.service.interfaces.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController

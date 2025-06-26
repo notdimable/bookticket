d71a931da61e52c251110128592bd414ac2e300c
notdimable <kevinsmithgpt@gmail.com>
2025-06-26 14:34:31 +0500

    Delete Users/Users directory

--- Users/Users/src/main/java/com/bookticket/app/api/users/service/interfaces/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.bookticket.app.api.users.service.interfaces;
-
-import com.bookticket.app.api.users.model.dto.UserDto;
-
-public interface UserService {
-    UserDto getDataUser(Long id);
-}
